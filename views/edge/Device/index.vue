f2659ae072f624a3894fb43ca20261e7ae08631a
XieYongHong <18010623010@163.com>
2025-10-24 11:49:16 +0800

    fix: 修改只有admin才能远程访问

--- views/edge/Device/index.vue
+++ views/edge/Device/index.vue
@@ -211,10 +211,12 @@ import Save from './Save/index.vue';
 import Import from '../../../components/Import/index.vue';
 import { useI18n } from 'vue-i18n';
 import DeviceCardImg from '@data-collector-ui/assets/device-card.png'
+import { useUserStore } from '@/store'
 
 const { t: $t } = useI18n();
 
 const menuStory = useMenuStore();
+const userStory = useUserStore();
 
 const defaultParams = {
     sorts: [{ name: 'createTime', order: 'desc' }],
@@ -470,6 +472,9 @@ const getActions = (
         },
     ];
 
+    if (!userStory.isAdmin) {
+      actions.splice(2,1)
+    }
 
     const deleteItem = {
         key: 'delete',
