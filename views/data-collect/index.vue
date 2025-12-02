6705d14ebb7700d0dceab2c23d4ac850ac81978f
sunchao <2642441182@qq.com>
2025-12-02 10:28:50 +0800

    feat: 新版数据采集器初始化

---
+++ views/data-collect/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <j-page-container>
+    <FullPage>
+      新版采集器,新的开始!!
+    </FullPage>
+  </j-page-container>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="less" scoped>
+
+</style>
